15ee360529694900379c3de720ee7c24694338af
ZenithClown <DebmalyaPramanik.005@gmail.com>
2020-12-21 14:06:39 +0530

    Implementation of Logging at CONSOLE
    * added YAML Logger configuration file
    
    * added MANIFEST.in for receognising \`*.yml\` file

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,33 @@
 # -*- encoding: utf-8 -*-
-# must require Python 3.6+ : (i) f-String Conventions, (ii) dotenv
+# must require Python 3.6+ : (i) f-String, (ii) dotenv
+
+import yaml
+import logging
+from logging.config import dictConfig
+
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
+
+from . import static
+
+# def logmaker():
+#     path = os.path.join("home", "debmalya", "pOrgz", "logs.log")
+#     return logging.FileHandler(path)
+
+def getLogger(loggerName : str = __name__):
+    """Defination of a Logger-Functionality - can be used to Track User Sessions and Login Information"""
+
+    # https://stackoverflow.com/questions/6028000/
+    # https://stackoverflow.com/questions/56688232/
+    loggerFile = pkg_resources.open_text(static, 'logger.yaml') # included static file in MANIFEST.in
+
+    # define logger using PyYAML
+    config = yaml.safe_load(loggerFile.read())
+    dictConfig(config)
+
+    return logging.getLogger(loggerName)
+
+# logger = getLogger()
--- app/main/controller.py
+++ app/main/controller.py
@@ -5,12 +5,18 @@ import time
 from uuid import uuid4
 from flask_restful import Resource
 
+from .. import getLogger
+
 class MyController(Resource):
     """A Controller defined for DEMO Purpose, returns static JSONs"""
+
+    def __init__(self):
+        self.logger = getLogger("INFOLogger-Console")
 
     def get(self):
         """Returns a Static JSON representing a GET requests"""
 
+        self.logger.info("GET, from MyController")
         return {
             "ID"      : str(uuid4()),
             "time"    : str(time.ctime()),
